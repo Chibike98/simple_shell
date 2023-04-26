239164976a3843d6a3fb39c3586e29305c3f81ad
Ebri Goodness <121ebrinix@gmail.com>
2023-04-26 11:29:39 -0700

    Update

---
+++ 1_shell.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * main - Simple Shell Command Prompt
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        while (1)
+        {
+                printf("#cisfun$ ");
+                fflush(stdout);
+
+                if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+                        break;
+
+                buffer[strcspn(buffer, "\n")] = '\0';
+
+                child_pid = fork();
+
+                if (child_pid == -1)
+                {
+                        fprintf(stderr, "%s: ", command);
+                        perror("");
+                        exit(1);
+                }
+
+                if (child_pid == 0)
+                {
+                        if (execve(args[0], args, NULL) == -1)
+                        {
+                                fprintf(stderr, "%s: ", command);
+                                perror("");
+                                exit(1);
+                        }
+                }
+                else
+                {
+                        waitpid(child_pid, &status, 0);
+                }
+        }
+
+        return (0);
+}
